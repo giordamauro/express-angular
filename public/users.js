e09079e8dfdba781d595891b31d8adb5187ddb94
Mauro Giorda <mgiorda@Mauros-Mac.local>
2016-07-05 19:40:31 -0300

    Add CRUD functionality

--- public/users.js
+++ public/users.js
@@ -1,8 +1,51 @@
 var app = angular.module("mgApp", []);
 
-app.controller("getUsers", function($scope, $http) {
-    $http.get('/api/users').
-        success(function(users) {
-            $scope.users = users;
-        });
+// Controllers;
+
+app.controller("UserController", function($scope, $http) {
+     
+    $scope.readUsers = function() {
+    	$scope.users = [{"_id": 123456, "firstName": "Mauro", "lastName": "Giorda", "age": 28, "gender": "M"}, {"_id": 654321, "firstName": "Jane", "lastName": "Doe", "age": 30, "gender": "F"}];
+	};
+
+	$scope.createUser = function(name, lastname, gender, age) {
+    	alert('created!');
+  	};
+
+	$scope.updateUser = function(name, lastname, gender, age) {
+    	alert('updated!');
+  	};
+
+    $scope.deleteUser = function() {
+    	alert('deleted user" + $scope.selectedUser._id + "!');
+  	};
+
+  	// Events:
+
+	$scope.onRowClicked = function(tableRow, row) {
+
+		$scope.selectedRow = row; 
+		$scope.selectedUser =  {};
+		$scope.selectedUser.firstName = tableRow.user.firstName;
+		$scope.selectedUser.lastName = tableRow.user.lastName;
+		$scope.selectedUser.gender = tableRow.user.gender;
+		$scope.selectedUser.age = tableRow.user.age;
+	};
 });
+
+// Directives;
+
+app.directive('ngConfirmClick', [
+  function(){
+  	return {
+    	link: function (scope, element, attr) {
+        	var msg = attr.ngConfirmClick || "Are you sure?";
+            var clickAction = attr.confirmedClick;
+            element.bind('click',function (event) {
+            	if ( window.confirm(msg) ) {
+              		scope.$eval(clickAction)
+            	}
+        	});
+    	}
+	};
+}])
